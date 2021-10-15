74055df58ef11f8891939a5964f71d7b71228887
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2021-10-15 17:59:17 +0530

    add readme.md and license

---
+++ README.md
@@ -0,0 +1,27 @@
+> boilerplate for basic-html-sass workflow
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-basic-html-boilerplate-with-scss using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-gulp-scss-html-boilerplate
+```
+
+Then generate your new project:
+
+```bash
+yo gulp-scss-html-boilerplate
+```
+
+## Getting To Know Yeoman
+
+- Yeoman has a heart of gold.
+- Yeoman is a person with feelings and opinions, but is very easy to work with.
+- Yeoman can be too opinionated at times but is easily convinced not to be.
+- Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Arbab Hussain](https://github.com/arbabhsiddiqui)
---
+++ generators/app/index.js
@@ -0,0 +1,38 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  welcome() {
+    this.log(
+      "Welcome To Gulp-scss-html boilerplate please wait while we are generating your boilerplate"
+    );
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath("_gitignore"),
+      this.destinationPath(".gitignore")
+    );
+    this.fs.copy(
+      this.templatePath("gulpfile.js"),
+      this.destinationPath("gulpfile.js")
+    );
+    this.fs.copy(
+      this.templatePath("package.json"),
+      this.destinationPath("package.json")
+    );
+
+    this.fs.copy(
+      this.templatePath("package-lock.json"),
+      this.destinationPath("package-lock.json")
+    );
+    this.fs.copy(this.templatePath("src"), this.destinationPath("src"));
+  }
+
+  end() {
+    this.log(" ");
+    this.log(
+      "Finished generating! All you need do is to  run npm init then copy all content of _devDependencies into package.json after author and run command npm i then run gulp  "
+    );
+    this.log(" ");
+  }
+};
